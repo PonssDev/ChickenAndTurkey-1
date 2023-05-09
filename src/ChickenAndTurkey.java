9c1c91bd0b9d539f0a99979e6f32fcadf5b3df0c
Degreco <antoniodaniel@estudiante.edib.es>
2023-05-09 19:35:53 +0200

    Initial commit

---
+++ src/ChickenAndTurkey.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ChickenAndTurkey {
+
+	public static void main(String[] args) {
+		 String username = "admin";
+	        String password = "chicken123";
+	        String urlBD = "jdbc:mysql://localhost:3306/chickenandturkey";
+	        try {
+	            Connection conn = DriverManager.getConnection(urlBD, username, password);
+			    System.out.println("Cargado correctamente");
+	            System.out.println("Conexión exitosa a la base de datos chickenandturkey.");
+	            conn.close();
+	        } catch (SQLException e) {
+	            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+	        }
+
+	}
+
+}
